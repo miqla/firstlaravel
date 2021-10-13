653a98671f5ecb08263b5af882a2685bed1b8296
miqla <miqdad.shaldin@gmail.com>
2021-10-13 23:48:32 +0700

    Membuat view login,  Membuat view registrasi, ngatur controllernya , route, view, dan sedikit css

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Post;
-use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Category;
 // use App\Models\Post;
 
-use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -73,3 +75,8 @@ Route::get('/categories', function() {
 //         'posts' => $author->posts->load('category','author')
 //     ]);
 // });
+
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
